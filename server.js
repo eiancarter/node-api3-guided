1fb84060187a5d3ec94172affd3d99b347b41420
Eian Carter <eian@bricleir.com>
2020-02-12 10:01:36 -0800

    added custom middleware for logger and greeting

--- server.js
+++ server.js
@@ -7,22 +7,29 @@ const server = express();
 
 // the three amigas: rachel, rita, nancy
 function logger(req, res, next) {
-
+  console.log(`request to ${req.method} and ${req.originalUrl}`)
+  next();
 }
 
-//middleware
+function greeter(req, res, next) {
+  req.cohort = 'web26';
+  next();
+}
+
+//global middleware
 server.use(express.json()); //built in middleware
-server.use(morgan('dev'));
+// server.use(morgan('dev'));
 server.use(helmet());
-server.use(logger());
+// server.use(logger);
+
 // routes- endpoints
 server.use('/api/hubs', hubsRouter);
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
 
+
+server.get("/", logger, greeter, (req, res) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome${nameInsert} to the Lambda Hubs API</p>
+    <p>Welcome ${req.cohort} to the Lambda Hubs API</p>
     `);
 });
 
